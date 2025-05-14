04dc25fbc7b11533a979700ac502868969bd2cdf
Michał Laszkowski <michal.laszkowski@agentic.pl>
2025-05-14 11:34:23 +0200

    SimpleSearch

---
+++ src/api/simple_search.py
@@ -0,0 +1,30 @@
+from sanic.log import logger
+from sanic.request import Request
+from sanic.views import HTTPMethodView
+from sanic.response import JSONResponse
+
+from .forms.search import SearchForm
+
+from src.services.cypher_search import simple_search
+
+
+class SimpleSearch(HTTPMethodView):
+    @staticmethod
+    async def post(request: Request) -> JSONResponse:
+        form = SearchForm(request.json)
+        if not form.is_valid():
+            return JSONResponse(body=form.errors, status=400)
+        query = form.cleaned_data['query']
+
+        response = simple_search(query)
+        if not response:
+            return JSONResponse(body={"error": "Error"}, status=404)
+
+
+        return JSONResponse(body=response)
+
+    @staticmethod
+    async def options(self, request: Request, *args, **kwargs) -> JSONResponse:
+        body = {'Access-Control-Allow-Methods': 'POST,OPTIONS'}
+        headers = {'Access-Control-Allow-Methods': 'POST,OPTIONS'}
+        return JSONResponse(body=body, headers=headers)
