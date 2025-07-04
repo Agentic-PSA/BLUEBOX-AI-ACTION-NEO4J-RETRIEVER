f7d92aea5d739a3020d2722cb4f1121b0eaf2a30
Michał Laszkowski <michal.laszkowski@agentic.pl>
2025-07-04 08:54:47 +0200

    get_product_specification env

--- src/services/product_specification.py
+++ src/services/product_specification.py
@@ -1,15 +1,22 @@
 from src.services.spiff_connector import SpiffConnector
+import os
 
 user = "spiffworkflow_backend"
 password = "spiffworkflow_backend"
 host = "172.19.3.220"
 port = "30432"
 database = "spiffworkflow_backend"
-connector = SpiffConnector(user, password, host, port, database)
+connector = SpiffConnector(user=os.environ.get("SPIFF_DB_USER"),
+                           password=os.environ.get("SPIFF_DB_PASSWORD"),
+                           host=os.environ.get("SPIFF_DB_HOST"),
+                           port=os.environ.get("SPIFF_DB_PORT"),
+                           database=os.environ.get("SPIFF_DB_DATABASE"))
 
 
 def get_product_specification(type):
-    return connector.get_value_from_data_store("original_specifications", "attributes", type.replace("_", "-"))
+    return connector.get_value_from_data_store(os.environ.get("SPECIFICATION_DATASTORE"),
+                                               "attributes",
+                                               type.replace("_", "-"))
 
 def filter_language(specification, language="PL"):
 
