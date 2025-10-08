26313664776e068b9f071e935f14fda22903e822
Cezary-Ziemiecki <cezary.ziemiecki@agentic.pl>
2025-10-08 14:25:26 +0200

    filter

--- src/services/cypher_search.py
+++ src/services/cypher_search.py
@@ -674,9 +674,7 @@ def cypher_search(user_query, return_parameters=False, ai_answer=False):
         try:
             start = time.time()
             specifications = {}
-            print('CCCCCCCCCCCCCCCCC_________________CCCCCCCCCCCCCCCCCCCC')
-            print(types)
-            print('DDDDDDDDDDDDDDDDD______________DDDDDDDDDDDDDDDDDDDDDDDDDDD')
+
             for t in types:
                 # print(f"Pobieranie specyfikacji dla typu: {t}")
 
@@ -914,8 +912,13 @@ def cypher_search(user_query, return_parameters=False, ai_answer=False):
         start = time.time()
         specifications = {}
         for t in types:
+            print('CCCCCCCCCCCCCCCCC_________________CCCCCCCCCCCCCCCCCCCC')
+            print(t, )
+            print('DDDDDDDDDDDDDDDDD______________DDDDDDDDDDDDDDDDDDDDDDDDDDD')
             # print(f"Pobieranie specyfikacji dla typu: {t}")
             specification = src.services.product_specification.get_product_specification(t)
+            print(specification, )
+            print('EEEEEEEEEEEEEEEEEEEEEE______________EEEEEEEEEEEEEEEEEEEEEE')
             if specification:
                 specification = src.services.product_specification.filter_language(specification, "PL")
                 specifications[type_to_label(t)] = specification
