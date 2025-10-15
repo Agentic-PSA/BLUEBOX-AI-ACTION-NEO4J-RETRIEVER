3962186011959e2c8a1319bad476809fe5244b44
Cezary-Ziemiecki <cezary.ziemiecki@agentic.pl>
2025-10-15 18:00:39 +0200

    url

--- src/services/product_specification.py
+++ src/services/product_specification.py
@@ -61,7 +61,7 @@ def get_form_data(column: str, value: str, table: str= 'forms') -> dict:
 def get_product_specification(type):
     category = get_form_data('category', type, table='category_to_type')
     print(category)
-    spec_data = get_form_data('category', category.get('type'))
+    spec_data = get_form_data('category', category.get('type'), table='forms')
 
     return spec_data['form'][0]['value']
     # print("services product_specification get_product_specification")
